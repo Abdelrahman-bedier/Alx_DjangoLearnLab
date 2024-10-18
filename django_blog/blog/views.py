caad10efcfa2eb322c123a87bef9a9b2552bfe64
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-10-18 20:05:14 +0300

    task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after successful registration
+            return redirect('home')  # Replace with your desired redirect after registration
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,8 +69,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
+LOGIN_REDIRECT_URL = 'profile'
 
+LOGOUT_REDIRECT_URL = 'login' 
+LOGIN_URL = 'login'  # Name of the login view
 
+# Logout URL
+LOGOUT_URL = 'logout' 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
